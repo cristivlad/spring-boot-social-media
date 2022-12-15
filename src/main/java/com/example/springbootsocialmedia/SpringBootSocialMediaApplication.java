9ee687b0f2eeba65a70c91760bdc40a348929724
cristiii <cristian.vlad10@gmail.com>
2022-12-15 21:38:02 +0200

    initial commit

---
+++ src/main/java/com/example/springbootsocialmedia/SpringBootSocialMediaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootsocialmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSocialMediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSocialMediaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootsocialmedia/SpringBootSocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootsocialmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSocialMediaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
