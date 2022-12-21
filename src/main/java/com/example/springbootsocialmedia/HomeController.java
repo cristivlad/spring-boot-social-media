aa35500491969fb53f95ab42ad680f4ffb7c0faa
cristiii <cristian.vlad10@gmail.com>
2022-12-21 20:34:03 +0200

    created endpoint to get an image

--- src/main/java/com/example/springbootsocialmedia/HomeController.java
+++ src/main/java/com/example/springbootsocialmedia/HomeController.java
@@ -1,14 +1,43 @@
 package com.example.springbootsocialmedia;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+import java.awt.*;
+import java.io.IOException;
 
 @RestController
+@RequiredArgsConstructor
 public class HomeController {
+
+    private static final String BASE_PATH = "/images";
+    private static final String FILENAME = "{filename:.+}";
+
+    private final ImageService imageService;
 
     @GetMapping
     public String greeting(@RequestParam(required = false, defaultValue = "") String name) {
         return "".equals(name) ? "Hey!" : "Hey, " + name + "!";
     }
+
+    @GetMapping(value = BASE_PATH + "/" + FILENAME + "/raw", produces = MediaType.IMAGE_JPEG_VALUE)
+    @ResponseBody
+    public Mono<ResponseEntity<?>> oneRawImage(@PathVariable String filename) {
+        return imageService.findOneImage(filename)
+                .map(resource -> {
+                    try {
+                        return ResponseEntity.ok()
+                                .contentLength(resource.contentLength())
+                                .body(new InputStreamResource(resource.getInputStream()));
+                    } catch (IOException e) {
+                        return ResponseEntity.badRequest()
+                                .body("Couldn't find " + filename + " => " + e.getMessage());
+                    }
+                });
+    }
+
 }
