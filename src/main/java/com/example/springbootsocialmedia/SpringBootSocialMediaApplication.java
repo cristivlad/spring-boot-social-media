2058873496dfb33da3f8adf6b1ea75c7fef21eff
cristiii <cristian.vlad10@gmail.com>
2022-12-21 21:01:13 +0200

    added HiddenHttpMethodFilter for the deleteMapping to work properly

--- src/main/java/com/example/springbootsocialmedia/SpringBootSocialMediaApplication.java
+++ src/main/java/com/example/springbootsocialmedia/SpringBootSocialMediaApplication.java
@@ -2,6 +2,8 @@ package com.example.springbootsocialmedia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 public class SpringBootSocialMediaApplication {
@@ -10,4 +12,8 @@ public class SpringBootSocialMediaApplication {
         SpringApplication.run(SpringBootSocialMediaApplication.class, args);
     }
 
+    @Bean
+    HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter();
+    }
 }
