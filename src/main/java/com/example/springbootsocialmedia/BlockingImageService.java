75bf921810c4844a98a15099599eb290d3de888d
cristiii <cristian.vlad10@gmail.com>
2022-12-22 23:15:07 +0200

    implementing async to sync endpoints

--- src/main/java/com/example/springbootsocialmedia/BlockingImageService.java
+++ src/main/java/com/example/springbootsocialmedia/BlockingImageService.java
@@ -1,9 +1,13 @@
 package com.example.springbootsocialmedia;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.codec.multipart.FilePart;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
 
+import static java.time.Duration.ofMinutes;
 import static java.time.Duration.ofSeconds;
 
 @RequiredArgsConstructor
@@ -14,6 +18,21 @@ public class BlockingImageService {
     public List<Image> findAllImages() {
         return imageService.findAllImages()
                 .collectList()
+                .block(ofSeconds(10));
+    }
+
+    public Resource findOneImage(String filename) {
+        return imageService.findOneImage(filename)
+                .block(ofSeconds(30));
+    }
+
+    public void createImage(List<FilePart> files) {
+        imageService.createImage(Flux.fromIterable(files))
+                .block(ofMinutes(1));
+    }
+
+    public void deleteImage(String filename) {
+        imageService.deleteImage(filename)
                 .block(ofSeconds(10));
     }
 }
